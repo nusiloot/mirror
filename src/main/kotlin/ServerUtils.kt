8e39cf84f5f0e90c7e50e64476f14a1f763cde0b
cak <cak@fastmail.com>
2020-10-15 06:28:15 -0400

    Proxy history load now only inscope

--- src/main/kotlin/ServerUtils.kt
+++ src/main/kotlin/ServerUtils.kt
@@ -1,12 +1,30 @@
 package burp
 
+import java.net.URL
+
 class ServerUtils {
     companion object {
         fun findRequest(
             url: String,
             callbacks: IBurpExtenderCallbacks
         ): IHttpRequestResponse? {
-            return callbacks.proxyHistory.asSequence().filter { filterProxyParams(it, url, callbacks) }
+            val uri = URL(url)
+            val host = uri.host
+            val port = if (uri.port != -1) {
+                uri.port
+            } else {
+                uri.defaultPort
+            }
+
+            val hostAndPort = "$host:$port"
+
+            val constructUrl = if (url.contains(":$port", ignoreCase = true)) {
+                url
+            } else {
+                url.replaceFirst(host, hostAndPort, ignoreCase = true)
+            }
+
+            return callbacks.proxyHistory.asSequence().filter { filterProxyParams(it, constructUrl, callbacks) }
                 .firstOrNull()
         }
 
